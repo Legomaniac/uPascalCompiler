cda11523a109b51d79d085c395f90438fa7dd7b6
James Blazicevich <jamesblazicevich@gmail.com>
2014-01-28 17:25:12 -0700

    Added file for scanner and set up basic structure
    Added scanner.py and basic skeleton to link everything together

---
+++ compyler.py
@@ -0,0 +1,15 @@
+import scanner
+
+"""
+microPascal compiler implemented in Python
+
+This is the main driver for the program
+"""
+
+def start():
+    print "We have started the compiler"
+
+
+if __name__ == "__main__":
+    import sys
+    start()
---
+++ scanner.py
@@ -0,0 +1,11 @@
+import tokens
+
+"""
+This is the scanner portion of our microPascal compiler
+"""
+
+#Scan things
+
+if __name__ == "__main__":
+    import sys
+    # Do something if the scanner is executed directly instead of imported
