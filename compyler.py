c5245bccf1047b35e79da7a1cc8498bd31bd153d
Justin Livingston <ascreamingweas@gmail.com>
2014-02-20 17:48:01 -0700

    Got compyler.py back to original scanning condition.

--- compyler.py
+++ compyler.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-import parser as parser
+import scanner as scanner
+from tokens import *
 import sys
 
 """
@@ -8,16 +9,24 @@ This is the main driver for the program
 """
 
 def main():
-    print "We have started the Parser."
+    print "We have started the compiler"
     fname = sys.argv[1]
     sourceText = open(str(fname)).read()
-    writeln("Token             Line    Col     Lexeme")
-    parser.parse(sourceText)
-    
-def writeln(*args): 
-    for arg in args: 
+    writeln("Here are the tokens returned by the scanner:")
+    writeln("Token Line Col Lexeme")
+    scanner.initialize(sourceText)
+    while True:
+        token = scanner.getToken()
+        if token is None:
+            print "Exception thrown! Quitting now..."
+            break
+        writeln(token.show())
+        if token.type == EPSILON: break
+                
+def writeln(*args):
+    for arg in args:
         print str(arg)
 
 if __name__ == "__main__":
     main()
-    print "Completed Parse!"
+    print "Completed run of Compiler."
